912b08492ee812c7173f44e0ef6d8e6e10e5d2c1
Prikshat2830 <prikshat384@gmail.com>
2026-05-23 09:51:35 +0530

    changes in bubble

--- src/components/MessageBubble/MessageBubble.jsx
+++ src/components/MessageBubble/MessageBubble.jsx
@@ -7,10 +7,20 @@ export default function MessageBubble({ message }) {
 
     const isMe = message.sender?._id === user?._id
 
+    const time = new Date(
+        message.timestamp || message.createdAt
+        ).toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit'
+        })
+
     return (
         <div className={isMe ? styles.meWrapper : styles.otherWrapper}>
             <div className={isMe ? styles.me : styles.other}>
                 {message.content}
+            </div>
+            <div className={styles.time}>
+                {time}
             </div>
         </div>
     )
